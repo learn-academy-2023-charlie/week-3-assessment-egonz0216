fcd9760a7b0d40e01b086a3783cdc1e60487057c
egonz0216 <egonz0216@gmail.com>
2023-05-06 14:30:27 -0700

    completed assignment, fixed the missing branch (hopefully)

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -12,56 +14,166 @@
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
+// Pseudo-Code:
+  // input: single number, which denotes the length of the array i want to output
+  // output: an array of numbers that follows the fibonacci sequence, with the length of the arrary equal to the value of the input
+  // Process: Create function "fibArr" that uses a for loop to iterate through the array i will create, and updates the value in every index as the sum of the previous two indexes -> i = (i-1)+(i-2). I will have a starting array with the beginging two values of the fib sequence- [1, 1], and use the for loop to build off of that. Will also include a condition to satisfy the requirement that the input number must be greater than two.
+
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+describe("fibArr", () => {
+  it("Takes in the length of the array and returns an array with every index as the sum of the previous two", () => {
+    const fibLength1 = 6
+    // Expected output: [1, 1, 2, 3, 5, 8]
+    const fibLength2 = 10
+    // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    expect(fibArr(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fibArr(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+})
+// FAIL  ./code-challenges.test.js
+// fibArr
+//   ✕ Takes in the length of the array and returns an array with every index as the sum of the previous two (4 ms)
+//   ReferenceError: fibArr is not defined
+
 
 // b) Create the function that makes the test pass.
+
+const fibArr = (num) => {
+  if (num < 2){
+    return "Invalid Input"
+  }
+  const fibStart = [1, 1]
+  for(let i = 2; i < num; i++) {
+    fibStart[i] = fibStart[i -1] + fibStart[i - 2]
+  } 
+  return fibStart
+}
+
+// PASS  ./code-challenges.test.js
+// fibArr
+//   ✓ Takes in the length of the array and returns an array with every index as the sum of the previous two (3 ms)
+
+
+
+
+
+
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
+// PseudoCode:
+  // Input: an object
+  // Output: an array of numbers, organized in ascending order
+  // process: I will use the object.values() class method to produce an array of the values in the key:value pairs presented in the objects, and organize the array in numerically ascending order using the sort method, (a - b) organizes them in ascending order
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
+describe ("numArr", () => {
+  it ("takes the values from the key value pairs and returns them in an array, sorted numerically", () => {
+    const studyMinutesWeek1 = {
+      sunday: 90,
+      monday: 30,
+      tuesday: 20,
+      wednesday: 15,
+      thursday: 30,
+      friday: 15,
+      saturday: 60
+    }
+    // Expected output: [15, 15, 20, 30, 30, 60, 90]
+    const studyMinutesWeek2 = {
+      sunday: 100,
+      monday: 10,
+      tuesday: 45,
+      wednesday: 60,
+      thursday: 20,
+      friday: 15,
+      saturday: 65
+    }
+    // Expected output: [10, 15, 20, 45, 60, 65, 100]
+    expect(numArr(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect(numArr(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
+})
+
+// FAIL  ./code-challenges.test.js
+// fibArr
+//   ✓ Takes in the length of the array and returns an array with every index as the sum of the previous two (5 ms)
+// numArr
+//   ✕ takes the values from the key value pairs and returns them in an array, sorted numerically (3 ms)
+//   ReferenceError: numArr is not defined
+
 
 // b) Create the function that makes the test pass.
+const numArr = (object) => {
+  return Object.values(object).sort((a, b) => a - b)
+}
+
+// PASS  ./code-challenges.test.js
+// fibArr
+//   ✓ Takes in the length of the array and returns an array with every index as the sum of the previous two (3 ms)
+// numArr
+//   ✓ takes the values from the key value pairs and returns them in an array, sorted numerically (2 ms)
+
+
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
+// PseudoCode:
+  // Input: the input will be an array of numbers
+  // Output: an array of the sums of the numbers in the input array
+  // Process: use .map to iterate through the array, and add the values from all of the previous indexes to the current index as the values in the new array. first, set conditional statement to account for an empty array. Then, declare a variable as the starting position, and use the addition assignment operator to add the values from the previosu indexes to the current one. 
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const accountTransactions1 = [100, -17, -23, -9]
-// Expected output: [100, 83, 60, 51]
+describe ("addArr", () => {
+  it ("sums every prior value in the array, and returns that sum in the following index", () => {
+    const accountTransactions1 = [100, -17, -23, -9]
+    // Expected output: [100, 83, 60, 51]
+    const accountTransactions2 = [250, -89, 100, -96]
+    // Expected output: [250, 161, 261, 165]
+    const accountTransactions3 = []
+    // Expected output: []
+    expect (addArr(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect (addArr(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect (addArr(accountTransactions3)).toEqual([])
+  })
+})
 
-const accountTransactions2 = [250, -89, 100, -96]
-// Expected output: [250, 161, 261, 165]
-
-const accountTransactions3 = []
-// Expected output: []
+// FAIL  ./code-challenges.test.js
+// fibArr
+//   ✓ Takes in the length of the array and returns an array with every index as the sum of the previous two (5 ms)
+// numArr
+//   ✓ takes the values from the key value pairs and returns them in an array, sorted numerically (10 ms)
+// addArr
+//   ✕ sums every prior value in the array, and returns that sum in the following index (3 ms)
+//   ReferenceError: addArr is not defined
 
 // b) Create the function that makes the test pass.
+
+const addArr = (arr) => {
+  if (arr.length === 0){
+    return []
+  }
+  let startingValue = arr[0]
+  return arr.map((value, index) => {
+    if (index === 0){
+      return value
+    }
+    startingValue += value
+    return startingValue
+  })
+}
+// PASS  ./code-challenges.test.js
+// fibArr
+//   ✓ Takes in the length of the array and returns an array with every index as the sum of the previous two (3 ms)
+// numArr
+//   ✓ takes the values from the key value pairs and returns them in an array, sorted numerically (2 ms)
+// addArr
+//   ✓ sums every prior value in the array, and returns that sum in the following index (1 ms)
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,48 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is the combination of HTML and Javascript that allows the display of a webpage to be dynamic and interactice. One difference in the syntax between the two is that in HTML, the tags are not case sensitive but in JSX they are. <div></DIV> in HTML, there would be no proplem within that tag, but in JSX that would return an arror message.
 
-Researched answer:
+Researched answer: JSX allows developers to write HTML code into their javscript files. With this combination however, some keywords must change because they mean something else in javascript. To avoid conflicts, a sustitute word is used - in javascript, we would use "class" to define a class, but in JSX we would use className.
 
 1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a resourse manager that the allows the user to summon pre-packaged code bits that have been created by other, very nice web developers to assist in the development of a webpage. Im not usre i undestand what the second part of the question is asking, but when we run yarn we modify thr root files that are the ones to get displayed on the webpage. 
 
-Researched answer:
+Researched answer: Yarn is a PACKAGE manager that reads the package.json file to determine which dependencies and files need to be installed. The json file is then updated with all of the dependencies that are added by yarn.
 
 3. What is a React component?
 
-Your answer:
+Your answer: A compartmentalization that helps keep our code clean, and prevents small mistakes from affecting the entirety of the code. we must then import the component to the main file, and summon it with a component call, to be visible on the webpage. 
 
-Researched answer:
+Researched answer: React components can be thought of as building blocks of the react application. They can be created with either a class declaration or a function declaration, and if using the functions we must use props as the input and return a react element. Components can be nested within other components, allowing for more modular and reusable code. 
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: Props in react are the method through which the parent component can send data to the child component. State values are the method we can use to make date updateable, but first we must also import {useState}
 
-Researched answer:
+Researched answer: State is a built in feature of react that represents the data within the component that can change over time, using the setState method. Props are short for properties, and are passed down from the parent component. Props are immutable, which means they are read-only, and cannot be modified by the child component
 
 5. What is the DOM?
 
-Your answer:
+Your answer: The DOM is the visual representation of the code on the webpage. I don't recall what the acronym stands for, but it organizes the data into a node tree, where each node where each node represents a single HTML element
 
-Researched answer:
+Researched answer: The Document Object Model- is used to identify which parts of the component's state or props are changed,and upddates the DOM in a process called reconcilliation. 
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
-Your answer:
+Your answer: A div tag is a way to to structure the data. With it, we can include different component calls in different orders, so long as they are included within the react fragments. I don't recall what the span tag does off the top of my head.
 
-Researched answer:
+Researched answer: Div is a block-level element, which means that it is a container that can group or pass stylistic or other data throughout the entirety of its container. It can be used on a large scale, such as the entiretiy of the web page, or even the entire document. A Span is similar in principle, but is used for smaller scale (in-line) application, such as to change a word, or phrase, or even a single character.
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: In OOP, programs are designed by creating objects and defining the interactions between them. 
 
-2. Ruby:
+2. Ruby: Is a dynamic, object oriented language. 
 
-3. Implicit return:
+3. Implicit return: a feature that allows a method to return a value without explicitly writing "return." Commonly used for one-liner methods where the return value is immediatly clear
 
-4. Ruby blocks:
+4. Ruby blocks: They are a way of passing code to a method as an arguement. When a method is called with a block, the block is executed within the context of teh method. Blocks are defined either using do/end or curly braces.
 
-5. Ruby hashes:
+5. Ruby hashes: They are a type of data structure that allows you to store key:value pairs. 
